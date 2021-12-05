b13f1219c5ff5067182c2f88aa020c85565eb263
Paul Davis <pauldavis@pitt.edu>
2021-12-05 01:24:25 -0800

    embed in movie page

--- theater-map.py
+++ theater-map.py
@@ -8,9 +8,13 @@ from folium.plugins import LocateControl, MarkerCluster
 MyMap = folium.Map(location=[47.6078, -122.3424], tiles='OpenStreetMap', zoom_start=11, max_zoom=13, control_scale=True)
 
 				#'Theater Name': [(lat, lon), 'theater type', 'website_URL', 'google_maps_directions_URL'],
-seattle_theaters = {'AMC Seattle 10': [(47.66175,-122.31811), 'amc', 'https://www.amctheatres.com/movie-theatres/seattle-tacoma/amc-seattle-10', 'google_maps_directions_URL'],
-                	'AMC Pacific Place 11': [(47.61315,-122.33553), 'amc', 'https://www.amctheatres.com/movie-theatres/seattle-tacoma/amc-pacific-place-11', 'google_maps_directions_URL'],
-                	'Regal Cinemas Thornton Place 14 & IMAX': [(47.70279,-122.32533), 'regal', 'https://www.regmovies.com/theatres/regal-thornton-place-imax/1937', 'google_maps_directions_URL'],
+seattle_theaters = {'AMC Seattle 10': [(47.66175,-122.31811), 'amc', 'https://www.amctheatres.com/movie-theatres/seattle-tacoma/amc-seattle-10', 'google_maps_directions_URL', '10'],
+                	'AMC Pacific Place 11': [(47.61315,-122.33553), 'amc', 'https://www.amctheatres.com/movie-theatres/seattle-tacoma/amc-pacific-place-11', 'google_maps_directions_URL', '11'],
+                	'Regal Cinemas Thornton Place 14 & IMAX': [(47.70279,-122.32533), 'regal', 'https://www.regmovies.com/theatres/regal-thornton-place-imax/1937', 'google_maps_directions_URL', '14'],
+                	'SIFF Cinema Egyptian': [(47.61509,-122.32173), 'siff', 'https://www.siff.net/siff-cinema/venues/siff-cinema-egyptian', 'google_maps_directions_URL', '1'],
+                	'SIFF Film Center': [(47.62312,-122.35319), 'siff', 'https://www.siff.net/siff-cinema/venues/siff-film-center', 'google_maps_directions_URL', '1'],
+                	'SIFF Cinema Uptown': [(47.62360,-122.35701), 'siff', 'https://www.siff.net/siff-cinema/venues/siff-cinema-uptown', 'google_maps_directions_URL', '3'],
+                	'Grand Illusion Cinema': [(47.66474,-122.31284), 'indie', 'https://grandillusioncinema.org', 'google_maps_directions_URL', '1'],
                 	}
 
 
@@ -22,13 +26,14 @@ for theater, details in seattle_theaters.items():
     theater_chain = details[1]
     website = details[2]
     directions = details[3]
-    insta_post = 'Hiya'
+    insta_post = 'no'
+    num_screens = details[4]
 
-    custom_icon = folium.CustomIcon('./{}.png'.format(theater_chain), icon_size=(35, 35), popup_anchor=(0, -22))
+    custom_icon = folium.CustomIcon('./movie_pictures/{}.png'.format(theater_chain), icon_size=(35, 35), popup_anchor=(0, -22))
    
     # Define html inside marker pop-up
     theater_html = folium.Html(f"""<p style="text-align: center;"><span style="font-family: Didot, serif; font-size: 21px;">{name}</span></p>
-    <p style="text-align: center;"><iframe src={insta_post}embed width="240" height="290" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
+    <p style="text-align: center;"><iframe src='./movie_pictures/FilmCenter.jpg'embed width="240" height="290" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>
     <p style="text-align: center;"><a href={website} target="_blank" title="{name} Website"><span style="font-family: Didot, serif; font-size: 17px;">{name} Website</span></a></p>
     <p style="text-align: center;"><a href={directions} target="_blank" title="Directions to {name}"><span style="font-family: Didot, serif; font-size: 17px;">Directions to {name}</span></a></p>
     """, script=True)
@@ -47,15 +52,8 @@ for theater, details in seattle_theaters.items():
 # Northwest Film Forum
 # Boeing IMAX at Pacific Science Center
 
-
-# Create custom icon
-
-
-# marker1 = folium.Marker(location=[47.66175,-122.31811], icon=amc_icon)
-# marker2 = folium.Marker(location=[47.61169,-122.33370], icon=regal_icon)
-# marker1.add_to(MyMap)
-# marker2.add_to(MyMap)
-
+# Add geolocation feature to map.
+LocateControl(auto_start=False).add_to(MyMap)
 
 # Save the map
 MyMap.save('theater-map.html')
